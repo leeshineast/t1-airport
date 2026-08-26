fb45110a0941ae8f73518c27354d374b699334c6
leeshineast <leeshineast@gmail.com>
2026-08-26 09:43:29 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -15,5 +15,15 @@ self.addEventListener('activate', function(e){
 
 self.addEventListener('fetch', function(e){
   // 항상 네트워크에서만 가져옴 (캐시 사용 안 함)
-  e.respondWith(fetch(e.request));
+  // 네트워크 실패 시에도 respondWith가 reject되지 않도록 처리
+  // (그렇지 않으면 브라우저가 "FetchEvent.respondWith received an error" 경고 배너를 띄움)
+  e.respondWith(
+    fetch(e.request).catch(function(err){
+      return new Response('오프라인 상태이거나 네트워크 연결에 실패했습니다.', {
+        status: 503,
+        statusText: 'Network Error',
+        headers: { 'Content-Type': 'text/plain; charset=utf-8' }
+      });
+    })
+  );
 });
